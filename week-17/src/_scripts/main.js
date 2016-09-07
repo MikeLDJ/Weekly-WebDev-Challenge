7ac512d539f8b27939bf18cc0bb3c3db948b6927
MikeLDJ <mikeu76@gmail.com>
2016-09-07 22:30:42 +0200

    carousel added

--- week-17/src/_scripts/main.js
+++ week-17/src/_scripts/main.js
@@ -5,7 +5,8 @@
 
 var $ = require('jquery');
 
-// Smooth Scrolling
+
+// --- Smooth Scrolling ---
 
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
@@ -19,5 +20,38 @@ $(function() {
         return false;
       }
     }
+  });
+});
+
+
+
+// --- Owl Carousel ---
+
+// fix for: Uncaught TypeError: Cannot read property 'fn' of undefined
+
+var jQuery = require('jquery');
+
+(function () {
+  var jq = jQuery.noConflict(true);
+  if (window.$ == undefined) {
+    window.$ = jq;
+  }
+  if (window.jQuery == undefined) {
+    window.jQuery = jq;
+  }
+})();
+
+//call owl
+
+$(document).ready(function(){
+  require('owl.carousel');
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    rewindSpeed: 100,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    loop: true,
+    navigation: false,
+    pagination: false
   });
 });
